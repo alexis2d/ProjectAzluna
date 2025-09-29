31cc39ade02e473471805e6eeb45401d5f14467f
Alexis Duquenet <alexisduquenet@gmail.com>
2025-09-29 20:37:51 +0200

    Base structure

---
+++ Assets/Scripts/Story/Dialogue.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+
+    private Character speaker;
+    private Character listener;
+    private string dialogueText;
+    private Expression expression;
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
